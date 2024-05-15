8861295b1d7723cb10abcbfdc38f73cbbcf23b6a
Sneha <ranisneha599@gmail.com>
2024-05-15 17:03:43 +0530

    make project section responsive

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,9 +7,9 @@ function Hero() {
         <section className={styles.containers}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hey, I&apos;m Sneha</h1>
-                <p className={styles.description}>As an enthusiastic frontend developer, I specialize in crafting engaging and responsive user interfaces using HTML, CSS, JavaScript, and React.js. My academic background and project experience have equipped me with a solid understanding of web development principles and best practices.
+                <p className={styles.description}>I specialize in crafting engaging and responsive user interfaces using HTML, CSS, JavaScript, and React.js. My academic background and project experience have equipped me with a solid understanding of web development principles and best practices.
                 </p>
-                <a className={styles.contactBtn} href="file:///C:/Users/Twinkle%20sigh/Downloads/Full%20name%20(1).pdf">Resume</a>
+                <a className={styles.contactBtn} href="../../assets/cv/cv.pdf">Resume</a>
             </div>
             <img className={styles.heroImg} src={getImgUrl("hero/heroImage.png")} alt="Hero Png" />
             <div className={styles.topBlur}/>
