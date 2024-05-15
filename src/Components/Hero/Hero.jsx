18c629273db760bc463b4c1087e710065f349398
Sneha <ranisneha599@gmail.com>
2024-05-15 17:27:23 +0530

    add cv path

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
                 <h1 className={styles.title}>Hey, I&apos;m Sneha</h1>
                 <p className={styles.description}>I specialize in crafting engaging and responsive user interfaces using HTML, CSS, JavaScript, and React.js. My academic background and project experience have equipped me with a solid understanding of web development principles and best practices.
                 </p>
-                <a className={styles.contactBtn} href="../../assets/cv/cv.pdf">Resume</a>
+                <a className={styles.contactBtn} href="../../../assets/cv/cv.pdf">Resume</a>
             </div>
             <img className={styles.heroImg} src={getImgUrl("hero/heroImage.png")} alt="Hero Png" />
             <div className={styles.topBlur}/>
