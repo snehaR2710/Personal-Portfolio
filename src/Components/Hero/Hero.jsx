b049da8a44628fffac2cab0e2e1c19bc19ce9283
Sneha <ranisneha599@gmail.com>
2024-05-20 17:55:26 +0530

    add cv

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import { getImgUrl } from "../../utils";
 import styles from './Hero.module.css';
+import cv from "../../assets/cv"
 
 
 function Hero() {
@@ -9,7 +10,7 @@ function Hero() {
                 <h1 className={styles.title}>Hey, I&apos;m Sneha</h1>
                 <p className={styles.description}>I specialize in crafting engaging and responsive user interfaces using HTML, CSS, JavaScript, and React.js. My academic background and project experience have equipped me with a solid understanding of web development principles and best practices.
                 </p>
-                <a className={styles.contactBtn} href="../../../assets/cv/cv.pdf">Resume</a>
+                <a className={styles.contactBtn} href={cv} target="_blank" rel="noopener noreferrer">Resume</a>
             </div>
             <img className={styles.heroImg} src={getImgUrl("hero/heroImage.png")} alt="Hero Png" />
             <div className={styles.topBlur}/>
